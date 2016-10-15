8c62eb458697f60050276bb68cbd2e65a2041388
Michael Nixon <zipplet@zipplet.co.uk>
2016-10-16 00:18:01 +0900

    Make autologin use common scripts

--- autologin/install.sh
+++ autologin/install.sh
@@ -7,24 +7,28 @@ BLUE='\033[1;34m'
 CYAN='\033[1;36m'
 NC='\033[0m'
 
-SDBOOT="/boot"
-NEWSDBOOT="/bootsd"
-USBDEVICE="/dev/sda"
-USBDEVICEPARTITION="${USBDEVICE}1"
-USBDEVICE2="/dev/sdb"
-TEMPMOUNT="/mnt/tempmount"
-CONFIG="/boot/cmdline.txt"
-CONFIGBACKUP="/boot/cmdline.txt.backup"
+INSTALLMARK="autologin"
+INSTALLMARKFILE="${INSTALLMARKDIR}/${INSTALLMARK}"
+
+if [ ! -e "../common/common.sh" ]; then
+  echo "Please run this script from the script directory."
+  exit 1
+else
+  . ../common/common.sh
+fi
 
 # We must run as root
-if [ "$(id -u)" != "0" ]; then
-   echo "${RED}Please run this installer as root${NC}" 1>&2
-   exit 1
+. $SCRIPT_CHECK_ROOT
+
+. $SCRIPT_CHECK_IF_INSTALLED
+if [ $INSTALLED -eq 1 ]; then
+  echo "${RED}${INSTALLMARK} is already installed.${NC}"
+  exit 1
 fi
 
 clear
 echo "${BLUE}---------------------------------${NC}"
-echo "${CYAN}autologin version 0.1 (20161012)${NC}"
+echo "${CYAN}autologin version 0.2 (20161015)${NC}"
 echo "${CYAN}Copyright (c) Michael Nixon 2016.${NC}"
 echo "${BLUE}---------------------------------${NC}"
 
@@ -44,7 +48,7 @@ if [ ! "$ANSWER" = "y" ]; then
   exit 1
 fi
 
-if [ ! -e "files/autologin@.service" ]; then
+if [ ! -f "files/autologin@.service" ]; then
   echo "${RED}Cannot find files/autologin@.service. Run this script from its own directory!${NC}"
   exit 1
 fi
@@ -88,6 +92,9 @@ echo "${GREEN}Setting up auto logon at boot...${NC}"
 cp files/autologin\@.service /etc/systemd/system/autologin\@.service
 rm /etc/systemd/system/getty.target.wants/getty@tty1.service
 ln -s /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+
+echo "${GREEN}Marking this tool as installed...${NC}"
+. $SCRIPT_MARK_AS_INSTALLED
 
 echo "${GREEN}All done. Your Pi will auto-login with the ${CYAN}autologin${GREEN} account now.${NC}"
 echo "${YELLOW}To undo this, run ${CYAN}undo.sh${NC} ${YELLOW}at any time.${NC}"
